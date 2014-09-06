9cc6882ce83d43d30f2960f81693652b7c9e4142
macbid <joe@macbidouille.com>
2014-09-06 13:49:49 +0200

    remove dead code

--- scripts.js
+++ scripts.js
@@ -2,12 +2,6 @@ $(window).resize(function(){
     $(".twitter-timeline").height($(window).height()-120);
 });
 
-
-$(document).ready(function() {
-	$(window).trigger('resize');
-});
-
-
 $(window).load(function(){
    function resizeTw(){
      $(window).trigger('resize');
