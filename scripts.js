6d1d49681ec158bec84d9b8c5f992b65478fa613
eric <eric@myqaa.com>
2014-09-09 09:24:57 +0200

    update pub background works

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 $(window).resize(function(){
-    $(".twitter-timeline").height($(window).height()-210);
+	$(".twitter-timeline").height($(window).height()-210);
 });
 
 $(window).load(function(){
-   function resizeTw(){
-     $(window).trigger('resize');
-   };
+	function resizeTw(){
+		$(window).trigger('resize');
+	};
    window.setTimeout(resizeTw, 500); // 1 second
-});
 
-$('body').click( function (e) {        
-    if ( this === e.target ) {
-        window.open	("http://macway.commander1.com/c3/?tcs=1377&chn=Display&src=Macbidouille&cmp=habillage&med=48&dis_pub=&dis_c=&dis_v=&dis_f=&url=http://www.macway.com/fr/?p=&utm_source=48&utm_campaign=macbidouille&utm_medium=habillage");
-    }
+   $('body').click( function (e) {  
+   	if ( this === e.target ) {
+   		window.open	("http://macway.commander1.com/c3/?tcs=1377&chn=Display&src=Macbidouille&cmp=habillage&med=48&dis_pub=&dis_c=&dis_v=&dis_f=&url=http://www.macway.com/fr/?p=&utm_source=48&utm_campaign=macbidouille&utm_medium=habillage");
+   	}
+   });
 });
