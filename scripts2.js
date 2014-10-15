62950b4a4a6b16c1b59d3188098e30012681081d
macbid <joe@macbidouille.com>
2014-10-15 21:04:28 +0200

    tuage des chats !

--- scripts2.js
+++ scripts2.js
@@ -15,8 +15,8 @@ $(window).load(function(){
 var first = true;
 
 var config = {
-  "id": '502160051226681344', //test avec photos de chat
-  //"id": '507966006153584640', //test fil macbidouille
+  //"id": '502160051226681344', //test avec photos de chat
+  "id": '507966006153584640', //test fil macbidouille
   "enableLinks": true,
   "maxTweets": 20,
   "showImages": true,
