929666ac066f2f945a614c476ba513c3a0502498
eric <eric@myqaa.com>
2014-10-16 16:51:47 +0200

    add banner and css updt

---
+++ banner.js
@@ -0,0 +1,8 @@
+$(window).load(function(){
+   $('body').click( function (e) {
+   	if ( this === e.currentTarget ) {
+
+   		window.open	("http://macway.commander1.com/c3/?tcs=1377&chn=Display&src=Macbidouille&cmp=habillage&med=48&dis_pub=&dis_c=&dis_v=&dis_f=&url=http://www.macway.com/fr/?p=&utm_source=48&utm_campaign=macbidouille&utm_medium=habillage");
+   	}
+   });
+});
--- scripts2.js
+++ scripts2.js
@@ -1,17 +1,3 @@
-/*
-$(window).resize(function(){
-    $(".twitter-timeline").height($(window).height()-210);
-});
-
-$(window).load(function(){
-   function resizeTw(){
-     $(window).trigger('resize');
-   };
-   window.setTimeout(resizeTw, 500); // 1 second
-})
-
-*/
-
 var first = true;
 
 var config = {
