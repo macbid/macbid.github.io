4d7a27055ba9979ece49e4b490853a6f9e3f6a93
eric <eric@myqaa.com>
2014-10-16 17:23:00 +0200

    URL banner and css updt

--- banner.js
+++ banner.js
@@ -2,7 +2,7 @@ $(window).load(function(){
    $('body').click( function (e) {
    	if ( this === e.currentTarget ) {
 
-   		window.open	("http://macway.com/redirect/partner/48?p=&utm_source=48&utm_campaign=macbidouille&utm_medium=habillage");
+   		window.open	("http://macway.com/redirect/partner/48");
    	}
    });
 });
