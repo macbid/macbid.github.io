0bb75056656e24dd0a48458c866295785a0439a8
eric <eric@myqaa.com>
2014-10-16 17:09:42 +0200

    URL banner and css updt

--- banner.js
+++ banner.js
@@ -2,7 +2,7 @@ $(window).load(function(){
    $('body').click( function (e) {
    	if ( this === e.currentTarget ) {
 
-   		window.open	("http://macway.commander1.com/c3/?tcs=1377&chn=Display&src=Macbidouille&cmp=habillage&med=48&dis_pub=&dis_c=&dis_v=&dis_f=&url=http://www.macway.com/fr/?p=&utm_source=48&utm_campaign=macbidouille&utm_medium=habillage");
+   		window.open	("http://macway.com/redirect/partner/48?p=&utm_source=48&utm_campaign=macbidouille&utm_medium=habillage");
    	}
    });
 });
