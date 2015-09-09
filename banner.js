3f781f5b55d386cd875feda47a6fa31c3f096a80
eric <eric@myqaa.com>
2015-09-09 08:19:21 +0200

    banner 25 ans macway ON

--- banner.js
+++ banner.js
@@ -2,7 +2,7 @@ $(window).load(function(){
    $('body').click( function (e) {
    	if ( this === e.currentTarget ) {
 
-   		window.open	("http://macway.com/redirect/partner/48");
+   		window.open	("http://macway.commander1.com/c3/?tcs=1377&chn=display&src=macbidouille&url=http://www.macway.com/fr/macway-expo?p=4&utm_source=4&utm_medium=BLG&utm_campaign=MacBidouille&utm_content=mwexpo");
    	}
    });
 });
